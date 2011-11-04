e00d0c42f5fb0a9b7f93004b7976a2dc1ddfbdb6
Goodwin Ogbuehi <github@gogbuehi.com>
2011-11-03 17:17:33 -0700

    Starting implementation of basic HTML editing

--- js/NewEntry.js
+++ js/NewEntry.js
@@ -1,23 +1,42 @@
 //Requires KeyManager.js
 var NewEntry = {
+        jModule:null,
+        jForm:null,
+        jDate:null,
+        jTime:null,
+        jContent:null,
+        jSubmit:null,
+        jCancel:null,
 	init:function() {
 		//Get the article element
-		var jModule = $("article.new_entry");
-		var jForm = jModule.find("form#new_entry");
-		var jDate = jForm.find("input.reference_date");
-		var jTime = jForm.find("input.reference_time");
-		var jContent = jForm.find("textarea.content_block");
-		var jSubmit = jForm.find("input.submit");
-		var jCancel = jForm.find("input.cancel");
-		if (jModule.length == 0) {
+		this.jModule = $("article.new_entry");
+		this.jForm = this.jModule.find("form#new_entry");
+		this.jDate = this.jForm.find("input.reference_date");
+		this.jTime = this.jForm.find("input.reference_time");
+		this.jContent = this.jForm.find("textarea.content_block");
+		this.jSubmit = this.jForm.find("input.submit");
+		this.jCancel = this.jForm.find("input.cancel");
+		if (this.jModule.length == 0) {
 			//Create one
 			//Add it to the jModule
 		}
-		jCancel.click(function() {
+		this.jCancel.click(function() {
 			//Clear the form and hide the entry form
-			jForm[0].reset();
-			jModule.hide('fast');
+			NewEntry.jForm[0].reset();
+			NewEntry.jModule.hide('fast');
 		});
 		
-	}
+                
+                
+                var jContent = $("article.content");
+                jContent.each(function() {
+                    $(this).click(function() {
+                        NewEntry.editEntry($(this));
+                    });
+                });
+	},
+        editEntry:function(jContent) {
+            this.jContent.html($(jContent).html());
+            this.jModule.show('fast');
+        }
 }
