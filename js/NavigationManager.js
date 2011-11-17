612d5f18e5443255325c2816259a8bc3ecd6d2ba
Goodwin Ogbuehi <github@gogbuehi.com>
2011-11-16 17:14:48 -0800

    Working on Binary Navigation

---
+++ js/NavigationManager.js
@@ -0,0 +1,12 @@
+var NavigationManager = {
+    jNavLeft:null,
+    jNavRight:null,
+    jNavMap:null,
+    init:function() {
+        jNavMap = $("div.navigator");
+        jNavLeft = $("div.left");
+        jNavRight = $("div.right");
+        
+        
+    }
+}
